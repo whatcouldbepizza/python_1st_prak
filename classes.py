53329dd043d8e9e7e46daf70ccacb3707177c45b
whatcouldbepizza <no_attention@mail.ru>
2019-10-16 11:13:56 +0300

    split classes.py into classes and gui files, added animation

--- classes.py
+++ classes.py
@@ -1,12 +1,4 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QSpacerItem, QSizePolicy
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-import matplotlib.pyplot as plt
-from matplotlib.patches import Arrow
-import multiprocessing
-
-
-Ui_MainWindow, QMainWindow = uic.loadUiType("form.ui")
+from matplotlib.patches import Circle
 
 
 class Particle:
@@ -22,6 +14,10 @@ class Particle:
         self.mass = mass
         self.color = color
         self.living_time = living_time
+        self.circle = Circle((coordinates[0], coordinates[1]), mass / 100, color=self.color)
+
+    def create_circle(self):
+        self.circle = Circle((self.coordinates[0], self.coordinates[1]), self.mass / 100, color=self.color)
 
 
 class Emitter:
@@ -41,145 +37,3 @@ class Emitter:
         """
         self.coordinates = coordinates
         self.vector = vector
-
-
-class MyMainWindow(QMainWindow, Ui_MainWindow):
-    """
-    Main window class
-    """
-    def __init__(self, ):
-        """
-        Main constructor
-        """
-        super(MyMainWindow, self).__init__()
-
-        self.setupUi(self)
-        self.connect_click_handlers()
-
-        self.emitter = Emitter()
-        self.particleList = []
-
-        self.figure = plt.figure()
-        self.canvas = FigureCanvas(self.figure)
-
-        self.setLayouts()
-
-        self.ax = self.figure.add_subplot(1, 1, 1)
-
-        #self.emitter_circle = plt.Circle((self.emitter.coordinates[0], self.emitter.coordinates[1]), 0.05, color='r')
-        self.emitter_vector = Arrow(self.emitter.coordinates[0], self.emitter.coordinates[1],
-                                    self.emitter.vector[0] / 20, self.emitter.vector[1] / 20, width=0.09)
-
-        #ax.add_artist(self.emitter_circle)
-        self.ax.add_artist(self.emitter_vector)
-
-        self.figure.canvas.mpl_connect('button_press_event', self.changeEmitter)
-
-
-
-
-    def connect_click_handlers(self):
-        """
-        Function that connects button click events to proper handlers
-        """
-        self.generateParticleButton.clicked.connect(self.generateParticle)
-        self.emitterChangeButton.clicked.connect(self.changeEmitter)
-
-
-    def setLayouts(self):
-        vertical_layout1 = QVBoxLayout()
-
-        vertical_layout1.addWidget(self.particleXAxisSpeedLineEdit)
-        vertical_layout1.addWidget(self.particleYAxisSpeedLineEdit)
-        vertical_layout1.addWidget(self.massLabel)
-        vertical_layout1.addWidget(self.particleMassSlider)
-        vertical_layout1.addWidget(self.generateParticleButton)
-
-        vertical_layout2 = QVBoxLayout()
-
-        vertical_layout2.addWidget(self.emitterXLineEdit)
-        vertical_layout2.addWidget(self.emitterYLineEdit)
-        vertical_layout2.addWidget(self.emitterXVectorLineEdit)
-        vertical_layout2.addWidget(self.emitterYVectorLineEdit)
-        vertical_layout2.addWidget(self.emitterChangeButton)
-
-        horizontal_layout = QHBoxLayout()
-
-        horizontal_layout.addLayout(vertical_layout1)
-        horizontal_layout.addLayout(vertical_layout2)
-        horizontal_layout.addWidget(self.canvas)
-
-        self.centralwidget.setLayout(horizontal_layout)
-
-
-    def generateParticle(self):
-        """
-        Generate particle button click event handler
-        """
-        try:
-            xAxisSpeed = int(self.particleXAxisSpeedLineEdit.text())
-        except Exception:
-            print("generateParticle(): x axis speed is in wrong format!")
-            return
-
-        try:
-            yAxisSpeed = int(self.particleYAxisSpeedLineEdit.text())
-        except Exception:
-            print("generateParticle(): y axis speed is in wrong format!")
-            return
-
-        mass = self.particleMassSlider.value()
-
-        particle = Particle(self.emitter.coordinates, [xAxisSpeed, yAxisSpeed], mass)
-
-        self.particleList.append(particle)
-
-
-    def changeEmitter(self):
-        """
-        Emitter position changed button click event handler
-        """
-        # ------------------------ Parsing new valus ---------------------------
-        try:
-            xAxis = float(self.emitterXLineEdit.text())
-        except Exception:
-            print("changeEmitter(): x axis is in wrong format!")
-            return
-
-        try:
-            yAxis = float(self.emitterYLineEdit.text())
-        except Exception:
-            print("changeEmitter(): y axis is in wrong format!")
-            return
-
-        try:
-            xVector = int(self.emitterXVectorLineEdit.text())
-        except Exception:
-            print("changeEmitter(): x axis vector is in wrong format!")
-            return
-
-        try:
-            yVector = int(self.emitterYVectorLineEdit.text())
-        except Exception:
-            print("changeEmitter(): y axis vector is in wrong format!")
-            return
-        # -----------------------------------------------------------------------
-
-        # Changing emitter position
-        self.emitter.change_position([xAxis, yAxis], [xVector, yVector])
-
-        # Removing old emitter (we have no emitter, don't do anything)
-        try:
-            self.emitter_vector.remove()
-        except Exception:
-            pass
-
-        # Creating new emitter
-        self.emitter_vector = Arrow(self.emitter.coordinates[0], self.emitter.coordinates[1],
-                                    self.emitter.vector[0] / 20, self.emitter.vector[1] / 20, width=0.09)
-        # Adding it to the plot
-        self.ax.add_artist(self.emitter_vector)
-
-        # Redraw plot
-        self.figure.canvas.draw_idle()
-
---
+++ gui.py
@@ -0,0 +1,179 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QSpacerItem, QSizePolicy
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import matplotlib.pyplot as plt
+from matplotlib.patches import Arrow
+from matplotlib.animation import FuncAnimation
+
+from scipy.integrate import odeint
+
+
+Ui_MainWindow, QMainWindow = uic.loadUiType("form.ui")
+
+
+class MyMainWindow(QMainWindow, Ui_MainWindow):
+    """
+    Main window class
+    """
+    def __init__(self, ):
+        """
+        Main constructor
+        """
+        super(MyMainWindow, self).__init__()
+
+        self.setupUi(self)
+        self.connect_click_handlers()
+
+        self.emitter = Emitter()
+
+        #manager = Manager()
+        #self.particleList = manager.list()
+        self.particleList = []
+
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+
+        self.setLayouts()
+
+        self.ax = self.figure.add_subplot(1, 1, 1)
+
+        self.emitter_vector = Arrow(self.emitter.coordinates[0], self.emitter.coordinates[1],
+                                    self.emitter.vector[0] / 20, self.emitter.vector[1] / 20, width=0.09)
+
+        self.ax.add_artist(self.emitter_vector)
+
+        self.figure.canvas.mpl_connect('button_press_event', self.changeEmitter)
+
+        self.animation = FuncAnimation(self.figure, self.draw_particles, interval=2000)
+
+
+    def draw_particles(self, frame):
+        """
+        Function to draw all particles
+        """
+        for i in range(len(self.particleList)):
+            try:
+                self.particleList[i].circle.remove()
+            except Exception:
+                pass
+
+            acceleration =
+
+            self.particleList[i].coordinates = [
+                self.particleList[i].coordinates[0] + self.particleList[i].speed[0],
+                self.particleList[i].coordinates[1] + self.particleList[i].speed[1]
+            ]
+
+            self.particleList[i].create_circle()
+
+            self.ax.add_artist(self.particleList[i].circle)
+
+        self.figure.canvas.draw_idle()
+
+
+    def connect_click_handlers(self):
+        """
+        Function that connects button click events to proper handlers
+        """
+        self.generateParticleButton.clicked.connect(self.generateParticle)
+        self.emitterChangeButton.clicked.connect(self.changeEmitter)
+
+
+    def setLayouts(self):
+        vertical_layout1 = QVBoxLayout()
+
+        vertical_layout1.addWidget(self.particleXAxisSpeedLineEdit)
+        vertical_layout1.addWidget(self.particleYAxisSpeedLineEdit)
+        vertical_layout1.addWidget(self.massLabel)
+        vertical_layout1.addWidget(self.particleMassSlider)
+        vertical_layout1.addWidget(self.generateParticleButton)
+
+        vertical_layout2 = QVBoxLayout()
+
+        vertical_layout2.addWidget(self.emitterXLineEdit)
+        vertical_layout2.addWidget(self.emitterYLineEdit)
+        vertical_layout2.addWidget(self.emitterXVectorLineEdit)
+        vertical_layout2.addWidget(self.emitterYVectorLineEdit)
+        vertical_layout2.addWidget(self.emitterChangeButton)
+
+        horizontal_layout = QHBoxLayout()
+
+        horizontal_layout.addLayout(vertical_layout1)
+        horizontal_layout.addLayout(vertical_layout2)
+        horizontal_layout.addWidget(self.canvas)
+
+        self.centralwidget.setLayout(horizontal_layout)
+
+
+    def generateParticle(self):
+        """
+        Generate particle button click event handler
+        """
+        try:
+            xAxisSpeed = float(self.particleXAxisSpeedLineEdit.text())
+        except Exception:
+            print("generateParticle(): x axis speed is in wrong format!")
+            return
+
+        try:
+            yAxisSpeed = float(self.particleYAxisSpeedLineEdit.text())
+        except Exception:
+            print("generateParticle(): y axis speed is in wrong format!")
+            return
+
+        mass = self.particleMassSlider.value()
+
+        particle = Particle(self.emitter.coordinates, [xAxisSpeed, yAxisSpeed], mass)
+
+        self.particleList.append(particle)
+
+
+    def changeEmitter(self):
+        """
+        Emitter position changed button click event handler
+        """
+        # ------------------------ Parsing new valus ---------------------------
+        try:
+            xAxis = float(self.emitterXLineEdit.text())
+        except Exception:
+            print("changeEmitter(): x axis is in wrong format!")
+            return
+
+        try:
+            yAxis = float(self.emitterYLineEdit.text())
+        except Exception:
+            print("changeEmitter(): y axis is in wrong format!")
+            return
+
+        try:
+            xVector = int(self.emitterXVectorLineEdit.text())
+        except Exception:
+            print("changeEmitter(): x axis vector is in wrong format!")
+            return
+
+        try:
+            yVector = int(self.emitterYVectorLineEdit.text())
+        except Exception:
+            print("changeEmitter(): y axis vector is in wrong format!")
+            return
+        # -----------------------------------------------------------------------
+
+        # Changing emitter position
+        self.emitter.change_position([xAxis, yAxis], [xVector, yVector])
+
+        # Removing old emitter (we have no emitter, don't do anything)
+        try:
+            self.emitter_vector.remove()
+        except Exception:
+            pass
+
+        # Creating new emitter
+        self.emitter_vector = Arrow(self.emitter.coordinates[0], self.emitter.coordinates[1],
+                                    self.emitter.vector[0] / 20, self.emitter.vector[1] / 20, width=0.09)
+        # Adding it to the plot
+        self.ax.add_artist(self.emitter_vector)
+
+        # Redraw plot
+        self.figure.canvas.draw_idle()
+
